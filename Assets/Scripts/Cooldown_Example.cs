70f7bacf1ee19bb6b80e81db462e2f1cc77c3f42
AndersFm-hue <andersmathiassen21@outlook.com>
2025-11-19 14:10:20 +0100

    Scripts

---
+++ Assets/Scripts/Cooldown_Example.cs
@@ -0,0 +1,62 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Cooldown_Example : MonoBehaviour
+{
+
+private SpriteRenderer sr;
+
+    [SerializeField] private float redColorDuration = 1;
+
+
+    public float curretTimeInGame;
+    public float lastTimeWasDamaged;
+
+    public float timer; 
+    private void Awake()
+
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+
+    private void Update()
+
+    {
+
+        curretTimeInGame = Time.time;
+
+        if (curretTimeInGame > lastTimeWasDamaged + redColorDuration)
+
+        {
+            if (sr.color != Color.white)
+                sr.color = Color.white;
+        }
+        
+
+    }
+
+    [ContextMenu("Update timer")]
+
+    private void UpdateTimer() => timer = redColorDuration;
+
+    public void TakeDamage()
+
+    {
+        sr.color = Color.red;
+        lastTimeWasDamaged = Time.time;
+
+
+    }
+
+
+    private void TurnWhite()
+
+    {
+        sr.color = Color.white;
+    }
+
+
+}
