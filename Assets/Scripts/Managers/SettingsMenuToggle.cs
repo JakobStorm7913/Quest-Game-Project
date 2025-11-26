49d38b9a3bf630626f1a3b573e8f3bf79e8fb56a
JakobStorm7913 <stormjakob4@gmail.com>
2025-11-26 14:22:25 +0100

    ome ui
    asse

--- Assets/Scripts/Managers/SettingsMenuToggle.cs
+++ Assets/Scripts/Managers/SettingsMenuToggle.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SettingsMenuToggle : MonoBehaviour
 {
     [SerializeField] private GameObject settingsPanel;
-    public void ShowSettings()
+    [SerializeField] private GameObject buttonSettings;
+    [SerializeField] private InputAction escapeAction;
+    [SerializeField] private bool escapeMenuShown = false;
+
+    void Awake()
     {
-        settingsPanel.SetActive(true);
-        Time.timeScale = 0f;
+        escapeAction = InputSystem.actions.FindAction("Escape");
+    if (escapeAction != null)
+    {
+        escapeAction.Enable();
+        escapeAction.performed += ctx => ShowAndHideSettings();
     }
-
-
-    public void HideSettings()
+    }
+    public void ShowAndHideSettings()
     {
+        if(!escapeMenuShown) {
+        settingsPanel.SetActive(true);
+        buttonSettings.SetActive(false);
+        Time.timeScale = 0f;
+        }
+        if(escapeMenuShown)
+        {
         settingsPanel.SetActive(false);
+        buttonSettings.SetActive(true);
         Time.timeScale = 1f;
+        }
     }
 }
